de459b380767238b3dac0ed8916a620701c49343
personalfridayassistant-oc <personalfridayassistant@gmail.com>
2026-03-13 10:04:48 -0400

    Fix automation menu action call to use VitaLite TClient API

---
+++ TestPlugins/finalflippingcopilot/FinalFlippingCopilotPlugin.java
@@ -0,0 +1,11 @@
+package com.finalflippingcopilot;
+
+import com.flippingcopilot.controller.FlippingCopilotPlugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+        name = "finalflippingcopilot",
+        description = "Flipping Copilot with built-in automation and no account gating"
+)
+public class FinalFlippingCopilotPlugin extends FlippingCopilotPlugin {
+}
